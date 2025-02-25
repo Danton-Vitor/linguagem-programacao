e4638da6b5c547bcd80db8a41f4909b5362848f5
Danton-Vitor <dantonvitor21@gmail.com>
2025-02-25 09:02:24 -0300

    meu primeiro programa web

--- js/scrips.js
+++ js/scrips.js
@@ -1,3 +1,16 @@
 function seguir(){
     alert(`Você agora está seguindo Danton Vitor`)
 }
+// exercício 1
+function exe1(){
+    // vamos usar DOM para recuperar os dados HTML
+    // DOM - Document Object Model 
+    //converter texto para número
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    // vamos fazer a subtração
+    let sub = nro1 - nro2
+    // vamos mostrar o resultado
+    //alert("A subtração é " + sub)
+    document.getElementById("sub").innerText = "O resultado é " + sub
+}
