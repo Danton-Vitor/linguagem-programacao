5ae257c848a3f2714e4c649cfe10b88f6fc1f7e6
Danton-Vitor <dantonvitor21@gmail.com>
2025-03-24 11:02:52 -0300

    exercício até o 10

--- Lista2/js/scrips.js
+++ Lista2/js/scrips.js
@@ -78,6 +78,18 @@ function exe5(){
         }
     }
 }
+function exe6(){
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    let selecao = Number(document.getElementById("selecao").value)
+    switch (selecao){
+        case 1: document.getElementById("resultado").innerText = "A potência é = " + Math.pow(nro1, nro2)
+        break
+        case 2: document.getElementById("resultado").innerHTML = "Raiz quadrada nº1 " + Math.sqrt(nro1).toFixed(2) + "<br/> Raiz quadrada nº2 " + Math.sqrt(nro2).toFixed(2)
+        break
+        case 3:  document.getElementById("resultado").innerHTML = "Raiz quadrada nº1 " + Math.cbrt(nro1).toFixed(2) + " <br/> Raiz quadrada nº2 " + Math.cbrt(nro2).toFixed(2)
+    }
+}
 function exe7(){
     let salario = Number(document.getElementById("salario").value)
     let aumento = Number(document.getElementById("aumento").value)
@@ -101,12 +113,67 @@ function exe8(){
         aumento = salario * 0.35
         novo_sal = salario + aumento
         document.getElementById("aumento").innerText = "Aumento R$" + aumento
-        document.getElementById("novo_sal").innerText = "Novo Salário R$" + novo_sal
+        document.getElementById("novo_sal").innerHTML = `Novo Salário R$ ${novo_sal}`
+
     }
     else {
         aumento = salario * 0.15
         novo_sal = salario + aumento
         document.getElementById("aumento").innerText = "Aumento R$" + aumento
-        document.getElementById("novo_sal").innerText = "Novo Salário R$" + novo_sal
+        document.getElementById("novo_sal").innerHTML = `Novo Salário R$ ${novo_sal}` //template string 
+    }
+}
+function exe9(){
+    let saldo_m = Number(document.getElementById("saldo_m").value)
+    let credito = Number(document.getElementById("credito").value)
+    if (saldo_m <= 200){
+        credito = saldo_m * 0.1
+        document.getElementById("saldo").innerText = "Saldo Médio R$" + saldo_m
+        document.getElementById("credito").innerText = "Crédito R$" + credito
+    }
+    else if (saldo_m > 200 && saldo_m <= 300){
+        credito = saldo_m * 0.2
+        document.getElementById("saldo").innerText = "Saldo Médio R$" + saldo_m
+        document.getElementById("credito").innerText = "Crédito R$" + credito
+    }
+    else if (saldo_m > 300 && saldo_m <= 400){
+        credito = saldo_m * 0.25
+        document.getElementById("saldo").innerText = "Saldo Médio R$" + saldo_m
+        document.getElementById("credito").innerText = "Crédito R$" + credito
+    }
+    else if (saldo_m > 400){
+        credito = saldo_m * 0.3
+        document.getElementById("saldo").innerText = "Saldo Médio R$" + saldo_m
+        document.getElementById("credito").innerText = "Crédito R$" + credito
+    }
+}
+function exe10(){
+    let custo = Number(document.getElementById("custo").value)
+    let imposto = Number(document.getElementById("imposto").value)
+    let distribuidor = Number(document.getElementById("distribuidor").value)
+    let preco = Number(document.getElementById("preco").value)
+    if (custo <= 12000){
+        distribuidor = custo * 0.05
+        imposto = "Isento"
+        preco = custo + distribuidor
+        document.getElementById("preco").innerText = "Preço Carro R$" + preco
+        document.getElementById("imposto").innerText = "valor Imposto: " + imposto
+        document.getElementById("distribuidor").innerText = "Valor Distribuidor R$: " + distribuidor
+    }
+    else if (custo < 25000){
+        distribuidor = custo * 0.1
+        imposto = custo * 0.15
+        preco = custo + distribuidor + imposto
+        document.getElementById("preco").innerText = "Preço Carro R$" + preco
+        document.getElementById("imposto").innerText = "Valor Imposto R$: " + imposto
+        document.getElementById("distribuidor").innerText = "Valor Distribuidor R$: " + distribuidor
+    }
+    else{
+        distribuidor = custo * 0.15
+        imposto = custo * 0.2
+        preco = custo + distribuidor + imposto
+        document.getElementById("preco").innerText = "Preço Carro R$" + preco
+        document.getElementById("imposto").innerText = "Valor Imposto R$: " + imposto
+        document.getElementById("distribuidor").innerText = "Valor Distribuidor R$: " + distribuidor
     }
 }
